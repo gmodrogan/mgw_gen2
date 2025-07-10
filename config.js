7793769710950ef5baae46af484c2c084e88ee06
George Modrogan <george.modrogan@cerner.com>
2025-07-10 14:02:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
- export const clientId = '1db6b5b1-0c9e-4a43-a356-f83be6d27c1f' //staging-int
+ export const clientId = 'bfa2fc9d-9a63-4d5c-b6a4-fa71ecaade49' //staging-int
 export const redirectUri = 'https://gmodrogan.github.io/mgw_gen2/after-auth'
 export const indexUri = 'https://gmodrogan.github.io/mgw_gen2/index'
 
