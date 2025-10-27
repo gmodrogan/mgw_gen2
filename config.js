b1f3d589141e247a58eec97aed89daef4fefb4c1
George Modrogan <gmodrogan@users.noreply.github.com>
2025-10-27 15:25:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
- export const clientId = 'bfa2fc9d-9a63-4d5c-b6a4-fa71ecaade49' //staging-int
+export const clientId = 'cc7f3d5f-213b-45a0-9645-af5250ba8943' //cert-int
+//export const clientId = 'bfa2fc9d-9a63-4d5c-b6a4-fa71ecaade49' //staging-int
 export const redirectUri = 'https://gmodrogan.github.io/mgw_gen2/after-auth'
 export const indexUri = 'https://gmodrogan.github.io/mgw_gen2/index'
 
